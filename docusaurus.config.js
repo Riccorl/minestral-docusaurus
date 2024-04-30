74ebd62277ed5a477663deee551eae387abab4c1
Edoardo Barba <edoardo.barba94@gmail.com>
2024-04-30 10:33:56 +0200

    index modification

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,58 +4,58 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Minestral',
-  tagline: 'Best LLM in town',
-  favicon: 'img/favicon.ico',
+  title: "Minerva Family",
+  tagline: "The first Italian-English LLMs trained from scratch!",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://riccardorlando.xyz/',
+  url: "https://riccardorlando.xyz/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/minestral-docusaurus',
+  baseUrl: "/minestral-docusaurus",
   trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Riccorl', // Usually your GitHub org/user name.
-  projectName: 'minestral-docusaurus', // Usually your repo name.
+  organizationName: "Riccorl", // Usually your GitHub org/user name.
+  projectName: "minestral-docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -65,72 +65,62 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: "Home",
         items: [
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Sapienza NLP`,
       },
       prism: {
         theme: prismThemes.github,
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,48 +1,71 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Minerva-350M-base-v1.0 ",
+    img: require("@site/static/img/minerva-logos-350M.png").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This compact model is <b>fast and agile</b>, making it ideal for
+        applications requiring quick responses and lower computational
+        resources. With dual-language training in Italian and English, it's{" "}
+        <u>perfectly suited for fine-tuning tasks</u> in specialized domains
+        where tailored responses are crucial.
       </>
     ),
+    link: "https://huggingface.co/sapienzanlp/Minerva-350M-base-v1.0",
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Minerva-1B-base-v1.0 ",
+    img: require("@site/static/img/minerva-logos-1B.png").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Featuring a <b>balanced blend of depth and speed</b>, this 1
+        billion-parameter model provides a robust framework for a variety of
+        applications. It strikes an effective balance between performance and
+        resource consumption, making it{" "}
+        <u>
+          suitable for developing more complex, multi-faceted language tasks
+          without extensive computational overhead.
+        </u>
       </>
     ),
+    link: "https://huggingface.co/sapienzanlp/Minerva-1B-base-v1.0",
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Minerva-3B-base-v1.0 ",
+    img: require("@site/static/img/minerva-logos-3B.png").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        This <b>powerful and comprehensive model</b> is trained on an extensive
+        corpus of Italian and English text, enabling sophisticated understanding
+        and generation of language. With its vast knowledge base and deep
+        learning capabilities, it is{" "}
+        <u>
+          optimized for delivering high-quality, general-purpose language
+          solutions
+        </u>{" "}
+        across a broad range of contexts and applications.
       </>
     ),
+    link: "https://huggingface.co/sapienzanlp/Minerva-3B-base-v1.0",
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ img, title, description, link }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img className={styles.featureSvg} src={img} alt="Example banner" />
       </div>
       <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+        <a href={link}>
+          <Heading as="h3">{title}</Heading>
+        </a>
+
         <p>{description}</p>
       </div>
     </div>
@@ -53,7 +76,28 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="text--center">
+          Minerva is the first family of{" "}
+          <b>
+            truly open (data and model) Italian-English LLMs pretrained from
+            scratch
+          </b>{" "}
+          developed by <a href="https://nlp.uniroma1.it/">Sapienza NLP</a> in
+          collaboration with{" "}
+          <a href="https://fondazione-fair.it/">
+            Future Artificial Intelligence Research (FAIR)
+          </a>{" "}
+          and <a href="https://www.cineca.it/">CINECA</a>.{" "}
+          <u>
+            Notably, approximately half of its pretraining data includes Italian
+            text!
+          </u>
+          <br />
+          If you want to know more about Minerva check the{" "}
+          <Link to="/blog/welcome">blogtechnical report blog post!</Link>
+        </div>
+
+        <div className="row" style={{ marginTop: "2rem" }}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
