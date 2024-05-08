0f3607aa503b5dfe70febe73eef0a046e17e109f
Simone Conia <conia@di.uniroma1.it>
2024-05-08 11:06:37 +0200

    Update baseUrl.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://riccardorlando.xyz/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/minestral-docusaurus",
+  baseUrl: "/minerva",
   trailingSlash: false,
 
   // GitHub pages deployment config.
